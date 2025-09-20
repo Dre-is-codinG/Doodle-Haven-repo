c5130f2df48be0dc5a9c8e3647dead24673d3d5c
Joshua Akintemi <joshuaakintemi3@gmail.com>
2025-09-20 15:22:04 +0100

    Fix saveAsSVG function to use blob upload and update dependencies

--- components/canvas.jsx
+++ components/canvas.jsx
@@ -12,6 +12,11 @@ import { Svg, Path } from "react-native-svg";
  Svg object creates a space that would allow the users to  draw on the canvas 
  Path object would allow me to track and record paths that are created as the user interacts with the Svg canvas component.
  */
+import { theme } from '../config/theme'; //imports theme object from config directory.
+// these imports would be accessing the firebase storage library objects that would be used to save images
+import { getStorage, getDownloadURL, uploadString, ref } from "firebase/storage";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { encode as btoa } from "base-64"
 
  const {height, width} = Dimensions.get('window')
 
@@ -20,9 +25,11 @@ const Canvas = () => {
 // this function would be used to create a state called paths, and using setPaths, continue to update it after a path has been set.
   const [currentPath, setcurrentPath] = useState([]);
 // the currentPath refers to the path the user is currently creating, this starts with a value of 0
-  
   const [bin, setbin] = useState([]);
 // the bin would be used to store sliced paths
+  const [showButton, setShowButton] = useState(false);
+// this function would be used to create a state that determines if a button is visible
+
 
   const handleFingerMove = (event) => {
     const newPath = [...currentPath];
@@ -125,11 +132,68 @@ const Canvas = () => {
     console.log("current Paths: ", currentPath)
   }
 
+  const createSVG = (paths, width, height) => {
+// this function takes in the paths array, the width and height of each stroke created as arguments
+    const svgPaths = paths // sets the variable svgPaths to the current state of the paths array
+    .map(pathArray => // the map method loops through the path array and converts these arrays into concactinated strings
+      `<path d="${pathArray.join('')} 
+        stroke="black"
+        fill="transparent"
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      `)// stores the styling and information about the strokes when being stored
+    .join('');
+/* 
+turns all the items into a singular string, this allows for the svg to be stored without being further 
+manipulated, and it would make it easier to recall the arrays 
+*/
+
+    return `
+    <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
+      ${svgPaths}
+    </svg>
+    `;
+/* 
+svg text is stored using xmlns which tells servers that the format of this text in memory is svg and the created
+svgPaths is called.
+*/
+  };
+
+  const saveAsSVG = async(paths, width, height) => {
+    try{
+      const svgPaths = createSVG(paths, width, height);// calls the svgPaths function created earlier
+      const storage = getStorage();// creates a variable that calls the firestore getStorage object
+      const fileName = `Art_${Date.now()}.svg`;// using backticks, i intend on saving all files with the Art_ prefix
+      const storageRef = ref(storage, `Art/${fileName}`);// references the storage where the svg path would be stored
+
+// this converts the svgPaths string to base 64 svg string, which is compatible with react native
+      const base64SVG = btoa(svgPaths);
+      const dataURI = `data:image/svg+xml;base64,${base64SVG}`;
 
 
-  return (
-    <ScrollView horizontal>
-      <View style={styles.container} onTouchMove={handleFingerMove} onTouchEnd={handleFingerMotionEnd}>
+      // this storage includes the name of the file and the location in which the file would be stored in firestore db
+      await uploadString(storageRef, dataURI, 'data_url');// saves as a data url instead of raw content
+      // uploads the raw string to the referenced location in storageRef, this tells firebase it is an svg file type
+      const downloadURL = await getDownloadURL(storageRef);// returns the dounload url used by storage reference
+
+      const db = getFirestore();
+// this references an already existing firestore, where if none is present, creates a new instance in firestore.
+      await addDoc(collection(db, "gallery"), {// uses the collection reference
+        url: downloadURL,
+        type: "svg",
+        createdAt: new Date(),
+        width: width,
+        height: height
+      });
+    } catch(err) {
+      console.error("Encountered error when trying to store svg, error: ", err)// throws any errors to the terminal
+    }
+  };
+  
+    return (
+      <ScrollView horizontal>
+        <View style={styles.container} onTouchMove={handleFingerMove} onTouchEnd={handleFingerMotionEnd}>
         <Svg style={styles.svgstyling}>
           <Path
           d = {paths.join(' ')} 
@@ -171,12 +235,31 @@ const Canvas = () => {
             style={styles.buttonImageStyle}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.functionButtons}>
+          <TouchableOpacity 
+          onPress={() => setShowButton(!showButton)}
+          style={styles.functionButtons}>
             <Image
             source={require('../assets/images/folder.png')}
             style={styles.buttonImageStyle}
             />
           </TouchableOpacity>
+          {/* conditional rendering of the two buttons when the save button is pressed */}
+          {showButton && (
+            <View style={styles.optionalViewStyling}>
+              <View style={styles.innerOptionalButtonView}>
+                <TouchableOpacity
+                onPress={() => saveAsSVG(paths, width, height)}
+                >
+                  <Text style={styles.optionalButtonTextStyling}>Save art to gallery!</Text>
+                </TouchableOpacity>
+              </View>
+                <TouchableOpacity
+                onPress={() => console.log("Art has been exported")}
+                >
+                  <Text style={styles.optionalButtonTextStyling}>Export art!</Text>
+                </TouchableOpacity>
+            </View>
+          )}
           <TouchableOpacity 
           onPress={clearFunction}
           style={styles.functionButtons}>
@@ -205,14 +288,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   toolBarStyling: {
-    width: width * 0.731,
+    width: width * 0.73,
     height: height * 0.3,
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: '#fff',
+    backgroundColor: theme.COLOURS.primary,
     marginHorizontal: 20,
     flexWrap: 'wrap', // allows for the tool buttons to go to the next line if they do not fit in the toolbar box.
-    flexDirection: 'row'
+    flexDirection: 'row', 
   },
   functionButtons: {
     width: width * 0.1,
@@ -227,5 +310,22 @@ const styles = StyleSheet.create({
   buttonImageStyle: {
     width: '50%',
     height: '50%'
+  },
+  optionalViewStyling: {
+  position: 'absolute',
+  bottom: -(height * 0.25),
+  left: (height * 0.68),
+  borderColor: '#000',
+  borderWidth: 2,
+  borderRadius: theme.BUTTONS.sharpButtonRadius,
+  },
+  optionalButtonTextStyling: {
+  fontFamily: theme.FONTS.formTitleFontFamily,
+  padding: 8,
+  fontSize: theme.FONTS.miniregularFontSize,
+  textAlign: 'center'
+  },
+  innerOptionalButtonView: {
+    borderBottomWidth : 2,
   }
 })
